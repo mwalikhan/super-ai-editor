6ffe7bd9a1bdc35c57049099eb779ce85f644a07
mwalikhan <hangu.studio@gmail.com>
2025-10-03 03:39:27 +0500

    Update MainActivity.kt

--- app/src/main/java/com/supereditor/ultimateeditor/MainActivity.kt
+++ app/src/main/java/com/supereditor/ultimateeditor/MainActivity.kt
@@ -1,12 +1,22 @@
-
 package com.supereditor.ultimateeditor
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-override fun onCreate(savedInstanceState: Bundle?) {
-super.onCreate(savedInstanceState)
-setContentView(R.layout.activity_main)
-}
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        
+        val textView = TextView(this)
+        textView.text = "Super AI Editor\nVersion 1.0"
+        textView.textSize = 24f
+        textView.setPadding(50, 50, 50, 50)
+        
+        setContentView(textView)
+        
+        val editorCore = EditorCore()
+        val version = editorCore.getAppVersion()
+    }
 }
