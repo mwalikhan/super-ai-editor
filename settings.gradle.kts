29f12e5b5dc0c3fd53d8e85a7e5b410ce8f60105
mwalikhan <hangu.studio@gmail.com>
2025-10-03 03:08:56 +0500

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,7 +11,6 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-        // Add JitPack for FFmpeg
         maven { url = uri("https://jitpack.io") }
     }
 }
