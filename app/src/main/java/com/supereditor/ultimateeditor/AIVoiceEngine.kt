26ee8fc9b0eeca581cdf4a445ebb20a16683aad7
mwalikhan <hangu.studio@gmail.com>
2025-10-03 04:10:41 +0500

    Update AIVoiceEngine.kt

--- app/src/main/java/com/supereditor/ultimateeditor/AIVoiceEngine.kt
+++ app/src/main/java/com/supereditor/ultimateeditor/AIVoiceEngine.kt
@@ -1,28 +1,20 @@
 package com.supereditor.ultimateeditor
 
-import java.io.File
+import android.util.Log
 
-class AIVoiceEngine(private val modelManager: ModelManager) {
-
-    private fun chunkText(script: String, chunkSize: Int = 1000) = script.chunked(chunkSize)
-
-    fun synthesizeUnlimited(script: String, emotion: String, language: String): String {
-        val modelPath = modelManager.getLocalModelPath("${language}_tts")
-            ?: return "No TTS model for $language"
-        val chunks = chunkText(script)
-        val outputs = mutableListOf<String>()
-
-        for ((i, part) in chunks.withIndex()) {
-            val fname = "tts_${language}_chunk_${i}.wav"
-            File(fname).writeText("VOICE[$language][$emotion]: $part")
-            outputs.add(fname)
-        }
-
-        val final = "tts_${language}_output.wav"
-        val listFile = File("concat.txt")
-        listFile.writeText(outputs.joinToString("\n") { "file '$it'" })
-
-        EditorCore().runFFmpeg("-f concat -safe 0 -i concat.txt -c copy $final")
-        return final
+class AIVoiceEngine {
+    
+    private val TAG = "AIVoiceEngine"
+    
+    fun generateVoice(text: String, outputPath: String): Boolean {
+        Log.d(TAG, "Generate voice: $text")
+        // Voice generation will be added later
+        return true
+    }
+    
+    fun addVoiceToVideo(videoPath: String, voicePath: String, outputPath: String): Boolean {
+        Log.d(TAG, "Add voice to video")
+        val command = "-i $videoPath -i $voicePath -c:v copy -c:a aac $outputPath"
+        return EditorCore.runFFmpeg(command)
     }
 }
