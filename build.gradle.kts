842e780edc73000bf44776ef745ce54513432682
mwalikhan <hangu.studio@gmail.com>
2025-10-03 04:01:27 +0500

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,3 @@ plugins {
     id("com.android.application") version "8.5.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
 }
-
-tasks.register("clean", Delete::class) {
-    delete(rootProject.layout.buildDirectory)
-}
