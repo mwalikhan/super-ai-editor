94eab3fc4969472d5f68ad530aac39ef09bbe8a7
mwalikhan <hangu.studio@gmail.com>
2025-10-03 03:25:19 +0500

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,16 +13,11 @@ android {
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
         release {
             isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
         }
     }
 
@@ -34,22 +29,11 @@ android {
     kotlinOptions {
         jvmTarget = "17"
     }
-    
-    buildFeatures {
-        viewBinding = true
-    }
 }
 
 dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
-    implementation("androidx.constraintlaint:constraintlayout:2.1.4")
-
-    // Mobile FFmpeg from JitPack
-    implementation("com.arthenica:mobile-ffmpeg-full:4.4.LTS")
-    
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 }
